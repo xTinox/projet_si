775bc93a0b5372b25b22ca7d8c124d53f1af90cb
requin-citron <requin_citron@protonmail.com>
2019-03-27 21:28:34 +0000

    Python OK

--- code_raspberry/raspberry.py
+++ code_raspberry/raspberry.py
@@ -4,22 +4,26 @@ import datetime
 import time
 class Arduino(SMBus):
     """docstring for Arduino."""
-    def __init__(self,addr):
+    def __init__(self,addr,path):
         super().__init__(1)
         self.addr=addr
         self.dico={0:self.send_allow_users}
+        self.path=path
     def __str__(self):
         return "Arduino sur l'address {}".format(self.addr)
-    def send_allow_users(self,path):
-        f=open(path,"r")
+    def send_allow_users(self):
+        f=open(self.path,"r")
         for i in f.readline():
             #on lie les uid et on enlève le \n (retour ligne) d'ou le [:2]
+            time.sleep(0.4)
             self.write_i2c_block_data(self.addr,1,i[:2].encode())
         self.write_byte(self.addr,2)
     def get_status(self):
         self.write_byte(self.addr,0)
+        tmp=self.read_byte(self.addr)
+        print(tmp)
         retour=self.dico.get(self.read_byte(self.addr))
         if(retour!=None): retour()
 
-arduino=Arduino(0x12)
+arduino=Arduino(0x12,"allow_users.txt")
 arduino.get_status()
