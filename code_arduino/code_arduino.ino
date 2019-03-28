9134947af328586ecc466dbd2123c3b4da27c8d7
requin-citron <requin_citron@protonmail.com>
2019-03-28 20:23:01 +0100

    C++ Ajoue

--- code_arduino/code_arduino.ino
+++ code_arduino/code_arduino.ino
@@ -1,35 +1,54 @@
+#include <SPI.h>
+#include <MFRC522.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#define SLAVE_ADDRESS 0x12
 
+#define SLAVE_ADDRESS 0x12 // arduino
+#define SS_PIN 10 //slave select RFID
+#define RST_PIN 9 //pin reset RFID
+#define LED_PIN_J 2 //pin led jaune
+#define LED_PIN_R 4 //pin led rouge
+#define BUZZER 7 //pin buzzer
 
-LiquidCrystal_I2C lcd(0x27,20,4);
+LiquidCrystal_I2C lcd(0x27,20,4); //on cree une instance de la class lcd
+MFRC522 rfid(SS_PIN, RST_PIN); // On instancie un MFRC522
+//decalaration des variable
 char separateur="-";
 int dataReceived = 0;
 bool flag_users=false;
-String buffer_iud="";
+String buffer_uid="";
+String buffer_uid_users="";
 String *allow_users=NULL;
 unsigned nb_allow_users=0;
 
 void setup() {
     Serial.begin(9600);
-    lcd.init();
-    lcd.backlight();
-    Wire.begin(SLAVE_ADDRESS);
-    Wire.onReceive(receiveData);
-    Wire.onRequest(sendData);
+    SPI.begin(); //On initialise la connection SPI
+    rfid.PCD_Init();//initialisation du lecteur rfid
+    lcd.init();//initialisation du lcd
+    lcd.backlight();//initialisation du retroéclairage
+    Wire.begin(SLAVE_ADDRESS);//initialisation de la liaison I2C
+    Wire.onReceive(receiveData);//pin de la fonction de reception
+    Wire.onRequest(sendData);//pin de la fonction d'envoie
 }
 
 void loop() {
-    if(flag_users){
-      Serial.println(flag_users);
-      lcd.clear();
-      while(1);
-      for(int i=0;i<nb_allow_users;i++){
-        Serial.println(allow_users[i]);
-      }
+    //on check que les iud soit importé
+    if(! flag_users){
+      wait();
+      return;
     }
-    else wait();
+    
+    buffer_uid="";//on vide le buffer
+    //nous vérifions si une carte est presente devant le lecteur
+    if(! rfid.PICC_IsNewCardPresent()) return;
+    //nous vérifions que l'uid est lu
+    if(! rfid.PICC_ReadCardSerial()) return;
+    //on lie l'uid de 4 octés
+    for(int i=0;i<4;i++){
+    //on ajoute les octés au buffer
+    buffer_uid += rfid.uid.uidByte[i];
+    }
     delay(200);
 }
 
@@ -55,14 +74,23 @@ void sendData(){
       Wire.write(1);
     }
 }
+//fonction de vérification element in tab
+bool check_in(String check,String tab[],int nb){
+  for(int i=0;i<nb;i++){
+    if(check==tab[i]){
+      return true;
+    }
+  }
+  return false;
+}
 //on cree la fontion de récuperation d'iud
 void recv_allow_users(){
           char buffer=0;
           for(int i=0;i<8;i++){
              buffer=Wire.read();
-             buffer_iud+=buffer;
+             buffer_uid_users+=buffer;
           }
-          buffer_iud+=separateur;
+          buffer_uid_users+=separateur;
           nb_allow_users++;
 }
 //on cree le tableau des utilisateur accepté
@@ -71,12 +99,12 @@ void init_allow_users(){
   int c=0;
   allow_users=(String*) calloc(nb_allow_users,sizeof(String));
   for(int i=0;i<(nb_allow_users*9);i++){
-    if(buffer_iud[i]==separateur){
+    if(buffer_uid_users[i]==separateur){
       allow_users[c]=buffer;
       buffer="";
       c++;
     }
-    else buffer+=buffer_iud[i];
+    else buffer+=buffer_uid_users[i];
   }
 }
 //fonction d'attente
@@ -85,7 +113,7 @@ void wait(){
   lcd.setCursor(1,0);
   lcd.print("Initialisation :");
   for(int i=0;i<17;i++){
-    delay(500);
+    delay(100);
     lcd.setCursor(i,1);
     lcd.print(".");
   }
