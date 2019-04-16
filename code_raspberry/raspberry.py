432363dd696dd9ad6644f1b9172e7f70933ff690
requin-citron <requin_citron@protonmail.com>
2019-04-16 23:04:04 +0200

    py

--- code_raspberry/raspberry.py
+++ code_raspberry/raspberry.py
@@ -36,7 +36,7 @@ class Arduino(SMBus):
             recv=self.read_i2c_block_data(self.addr,2,8)
             print(recv)
             if recv==[0 for x in range(8)]:
-                break;
+                break
             for i in range(len(recv)):
                 uid+=chr(recv[i])
             f.write(str(uid)+"-"+str(now.year)+"-"+str(now.hour)+"-"+str(now.minute)+"-"+str(uid in self.list)+"\n")
